7b3d70227424cd69c961ce31381f19036f50b8da
spidlfil <spidlfil@cvut.cz>
2022-05-08 19:30:32 +0200

    basic frame of program

---
+++ semestralka02/semestralka02/src/program.cpp
@@ -0,0 +1,76 @@
+
+#include "program.hpp"
+#include <iostream>
+
+
+//private methods
+
+bool program::startScreen() {
+
+}
+
+bool program::showLevels() {
+
+}
+
+
+bool program::loadLevel(std::string name){
+
+}
+
+
+bool program::loadGrid(){
+
+}
+
+
+bool program::loadBombers() {
+
+}
+
+bool program::saveResults(const int & newBest) {
+
+}
+
+
+//public methods
+
+program::program() {
+
+}
+
+program::~program() {
+
+}
+
+bool program::run(){
+
+	if (!startScreen()) {
+		std::cout << "error has occured while starting program " << std::endl;
+		return false;
+	}
+
+	int achievedScore = game.doGame();
+
+	if (achievedScore < 0) {		 //an error has occured in game
+		std::cout << "error has occured while playing game, err: " << achievedScore << std::endl;
+		return false;
+	}
+
+
+	if (currBestScore < achievedScore)
+		std::cout << "saving new best score: " << currBestScore << std::endl;
+
+		if (!saveResults(achievedScore)) {
+			std::cout << "error while saving results" << std::endl;
+			return false;
+			}
+
+
+
+	return true;
+
+
+
+
+}
---
+++ semestralka02/semestralka02/src/program.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "game.hpp"
+#include <string>
+
+class program {
+
+	/*
+	Manages life of whole program
+	*/
+	std::string fileNameLevel;
+	game game;
+
+
+	/**
+	 * Starting screen shown to player.
+	 * 
+	 * \return False if screen fails to load, other than that True
+	 */
+	bool startScreen();
+
+
+	/**
+	 * Loads levels in level directory.
+	 * 
+	 * \return False in case no levels found, other than that True
+	 */
+	bool showLevels();
+
+
+	/**
+	 * Attemts to load level based on its name.
+	 * 
+	 * \param name, name of the level
+	 * \return False if any part failed to load, else True
+	 */
+	bool loadLevel(std::string name);
+
+	/**
+	 * Attemts to load grid into game instance.
+	 * 
+	 * \return False if failed to load grid, else True
+	 */
+	bool loadGrid();
+
+
+	/**
+	 * Loads bombers based on level and player input. Player gets to choose Which mode the robots in level will be using
+	 * They can either be controlled by player, not move at all, or be controlled by computer
+	 * 
+	 * \return False if any bomber failed to load, else True
+	 */
+	bool loadBombers();
+
+
+	/**
+	 * Method called after game is finished.
+	 * Can save achieved results if if should
+	 * 
+	 * \return False if fails to save for any reason, else True
+	 */
+	bool saveResults();
+
+public:
+
+
+	program();
+	~program();
+
+	/**
+	 * Managing method of all the parts of program.
+	 * Checks if parts are running properly, in case of failure stops the program and cleans up
+	 * 
+	 * \return True if program ran its course without issues, False if program had to stop unexpectedly
+	 */
+	bool run();
+};
